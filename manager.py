fb49f926bf09750cb6d1de76165d8a70a16ac397
ssgll <guo.orm.o@gmail.com>
2020-07-29 21:06:29 +0800

    A basic project framework

---
+++ manager.py
@@ -0,0 +1,14 @@
+from flask import Flask
+# from config import config
+
+from app.urls import indexBlueprint
+
+def createApp():
+	app = Flask(__name__)
+	app.register_blueprint(indexBlueprint)
+
+	return app
+
+app = createApp()
+
+app.run()
